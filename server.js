b195b10752ab39105c907d048b0b2fcaba66236f
runlaiyang <yangrunlai@vip.qq.com>
2023-08-22 15:25:15 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Standard App - trigger CICD');
+  res.send('Standard App - trigger CICD for 2 services');
 });
 
 app.listen(port, () => {
